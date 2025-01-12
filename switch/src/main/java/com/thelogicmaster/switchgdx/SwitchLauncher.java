9b25d91edcab169dd12fd96b12f2fe1c645f6945
Justin Marentette <justinmarentette11@gmail.com>
2025-01-11 21:02:06 -0500

    Switch port

---
+++ switch/src/main/java/com/thelogicmaster/switchgdx/SwitchLauncher.java
@@ -0,0 +1,10 @@
+package com.thelogicmaster.switchgdx;
+
+import com.quillraven.game.MysticGarden;
+
+public class SwitchLauncher {
+
+	public static void main (String[] arg) {
+		new SwitchApplication(new MysticGarden());
+	}
+}
